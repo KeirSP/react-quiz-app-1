aabbcdf34d66d3dbc90dfb66cb26ce6d72610295
huseyin <thehusko@gmail.com>
2020-04-27 12:54:48 +0100

    created a form, updates state, to setup quiz

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Setup from "./components/quizsetup"
+import {BrowserRouter, Switch, Route} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Setup} />
+    </Switch>
+  </BrowserRouter>
   );
 }
 
---
+++ src/components/quizsetup.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
+class Setup extends Component {
+    constructor(props){
+        super(props);
+        this.state = { 
+            questionAmount: "5",
+            category: "9",
+            difficulty: "easy",
+            numOfPlayers: "1"
+         }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+
+     handleChange(event) {
+        event.preventDefault()
+        const value = event.target.value;
+        console.log(value)
+        this.setState({
+            ...this.state,
+            [event.target.name]: value
+         });
+        console.log(this.state)
+      }
+
+      handleSubmit(event) {
+          event.preventDefault()
+
+        };
+
+    render() { 
+        return ( 
+            <form onSubmit={this.handleSubmit}>
+                <label htmlFor="difficultyDropdown">Difficulty</label>
+                    <select
+                    name = "difficulty"
+                    className="difficultyDropDown"
+                    value = {this.state.difficulty}
+                    onChange={this.handleChange}
+                    >
+                        <option value="easy" defaultValue="selected">Easy</option>
+                        <option value="medium">Medium</option>
+                        <option value="hard">Hard</option>
+                    </select>
+                <label htmlFor="categoryDropdown">Category</label>
+                    <select
+                    name = "category"
+                    className="categoryDropDown"
+                    value = {this.state.category}
+                    onChange={this.handleChange}
+                    >
+                        <option value="9" defaultValue="selected">General Knowledge</option>
+                        <option value="21">Sports</option>
+                        <option value="17">Science & Nature</option>
+                        <option value="23">History</option>
+                        <option value="28">Vehicles</option>
+                    </select>
+                <label htmlFor="numberOfQuestionsDropdown">Number of questions</label>
+                    <select
+                    name = "questionAmount"
+                    className="numberOfQuestionsDropdown"
+                    value = {this.state.questionAmount}
+                    onChange={this.handleChange}
+                    >
+                        <option value="5" defaultValue="selected">5</option>
+                        <option value="10">10</option>
+                        <option value="15">15</option>
+                    </select>
+                <label htmlFor="numberOfPlayersDropdown">Number of players</label>
+                    <select
+                    name = "numOfPlayers"
+                    className="numberOfPlayersDropdown"
+                    value = {this.state.questionAmount}
+                    onChange={this.handleChange}
+                    >
+                        <option value="1" defaultValue="selected">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                    </select>
+            </form>
+         );
+    }
+}
+
+
+export default Setup;
