0913f53b38ff18626c47bf03714d641b03dccbee
huseyin <thehusko@gmail.com>
2020-04-27 17:26:06 +0100

    displaying questions, need to randomise

--- src/components/Question.js
+++ src/components/Question.js
@@ -5,8 +5,10 @@ class Question extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            errorMessage: ""
-            
+            errorMessage: "",
+            questions: [],
+            userOneAnswers: [],
+            userTwoAnswers: []
          }
     }
 
@@ -16,8 +18,9 @@ class Question extends Component {
         const category = this.props.location.state.category;
         const difficulty = this.props.location.state.difficulty;
         try{
-            const apiResponse = await axios(`https://opentdb.com/api.php?amount=${questionAmount}&category=${category}&difficulty=${difficulty}`)
-            console.log(apiResponse)
+            const apiResponse = await axios(`https://opentdb.com/api.php?amount=${questionAmount}&category=${category}&difficulty=${difficulty}&type=multiple`)
+            this.setState({questions:apiResponse.data.results})
+            console.log(this.state.questions)
         }
         catch(err){
             this.setState({errorMessage:err})
@@ -25,12 +28,36 @@ class Question extends Component {
     }
 
     render() { 
-        return ( 
+        const questionItems = this.state.questions;
+        console.log(questionItems)
+        return (
             <div>
-                <h2>Hi</h2>
+                {questionItems.length > 0 ?
+                
+                questionItems.map(item => (
+                    <div>
+                    <h5>{item.question}</h5>
+                    <br />
+                    <label>{item.correct_answer}</label>
+                    <input type="radio" name="question" id={item.index} value={item.correct_answer} />
+                    <label>{item.incorrect_answers[0]}</label>
+                    <input type="radio" name="question" id={item.index} value={item.incorrect_answers[0]} />
+                    <label>{item.incorrect_answers[1]}</label>
+                    <input type="radio" name="question" id={item.index} value={item.incorrect_answers[1]} />
+                    <label>{item.incorrect_answers[2]}</label>
+                    <input type="radio" name="question" id={item.index} value={item.incorrect_answers[2]} />
+                    </div> 
+                ))
+                
+                :
+                <h3>Loading</h3>}
             </div>
          );
     }
 }
- 
+
+
+
+
+
 export default Question;
